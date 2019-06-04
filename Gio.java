0c9623454f1f52d6dba77b737881aefe3fa32bd8
Victor_Grecca <31428776+VictorGrecca@users.noreply.github.com>
2019-06-04 11:44:06 -0300

    Add files via upload

---
+++ Gio.java
@@ -0,0 +1,24 @@
+package gibes_bot.giobot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Gio extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotToken() {
+    
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+    }
+
+    @Override
+    public String getBotUsername() {
+    
+        return null;
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package gibes_bot.giobot;
+
+public class Main{
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
